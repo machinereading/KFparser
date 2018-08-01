f6b94e9f887d096bb10dc2a3b3459375178f34f0
hahmyg <hahmyg@gmail.com>
2018-08-01 17:51:14 +0900

    lstm model

--- src/frameid_lstm.py
+++ src/frameid_lstm.py
@@ -11,6 +11,7 @@ import torch.optim as optim
 import os
 import sys
 import os
+import pprint
 os.environ["CUDA_VISIBLE_DEVICES"]= "0"
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 import torch.backends.cudnn as cudnn
@@ -23,22 +24,28 @@ import time
 start_time = time.time()
 
 torch.manual_seed(1)
-print('GPU:', torch.cuda.get_device_name(0), ', # of gpu:('+str(torch.cuda.device_count())+')')
 
 
 # In[2]:
 
 
-training_data, test_data, dev_data, exemplar_data = preprocessor.load_data()
+print('GPU:', torch.cuda.get_device_name(0), ', # of gpu:('+str(torch.cuda.device_count())+')')
+print('Torch Version:', torch.version.cuda)
 
 
 # In[3]:
 
 
-preprocessor.data_stat()
+training_data, test_data, dev_data, exemplar_data = preprocessor.load_data()
 
 
 # In[4]:
+
+
+preprocessor.data_stat()
+
+
+# In[5]:
 
 
 configuration = {'token_dim': 60,
@@ -51,36 +58,34 @@ configuration = {'token_dim': 60,
                  'lstm_depth': 2,
                  'hidden_dim': 64,
                  'num_epochs': 25,
-                 'learning_rate': 0.0005}
-
-
-# In[5]:
-
-
-model_path = './model/lstm180731'
+                 'learning_rate': 0.0005,
+                 'using_GPU': True}
+print('\n### CONFIGURATION ###\n')
+pprint.pprint(configuration)
 
 
 # In[6]:
 
 
+model_path = './model/lstm180731'
+
+
+# In[7]:
+
+
 #Hyper-parameters
-#TOKDIM = configuration['token_dim']
+usingGPU = configuration['using_GPU']
 TOKDIM= configuration['token_dim']
-
-
 POSDIM = configuration['pos_dim']
 LUDIM = configuration['lu_dim']
 LPDIM = configuration['lu_pos_dim']
 INPDIM = LUDIM + LPDIM
-
 LSTMINPDIM = configuration['lstm_input_dim']
 LSTMDIM = configuration['lstm_dim']
 LSTMDEPTH = configuration['lstm_depth']
 HIDDENDIM = configuration['hidden_dim']
-
 # TOKDIM = 6
 # HIDDENDIM =6
-
 NUM_EPOCHS = configuration['num_epochs']
 learning_rate = configuration['learning_rate']
 
@@ -91,7 +96,7 @@ learning_rate = configuration['learning_rate']
 # seq_length = 30
 
 
-# In[7]:
+# In[8]:
 
 
 def prepare_index():
@@ -116,24 +121,30 @@ print('###  word vocab size:', len(word_to_ix))
 print('### frame vocab size:', len(frame_to_ix))
 
 
-# In[8]:
+# In[9]:
 
 
 def prepare_sequence(seq, to_ix):
     idxs = [to_ix[w] for w in seq]
-    return torch.tensor(idxs).type(torch.cuda.LongTensor)
+    if usingGPU:
+        return torch.tensor(idxs).type(torch.cuda.LongTensor)
+    else:
+        return torch.tensor(idxs, dtype=torch.long)
 
 
-# In[9]:
+# In[10]:
 
 
 def prepare_frame_sequence(seq, to_ix):
     idxs = [to_ix[w] for w in seq if w != '_']
     idxs = list(set(idxs))
-    return torch.tensor(idxs).type(torch.cuda.LongTensor)
+    if usingGPU:
+        return torch.tensor(idxs).type(torch.cuda.LongTensor)
+    else:
+        return torch.tensor(idxs, dtype=torch.long)
 
 
-# In[10]:
+# In[11]:
 
 
 def prepare_sentence(tokens):
@@ -150,7 +161,7 @@ def prepare_sentence(tokens):
     return sentence, pos, frame
 
 
-# In[11]:
+# In[12]:
 
 
 def get_targetpositions(tokens):
@@ -161,27 +172,20 @@ def get_targetpositions(tokens):
     return positions
 
 
-# In[12]:
+# In[13]:
 
 
 def get_target_span(sentence, targetpositions):
     start, end = targetpositions[0], targetpositions[-1]
-#     lenth = len(targetpositions)
     span = {}
-    if start == 0:
-        span['start'] = 0
-    else:
-        span['start'] = start -1
-#         lenth = lenth+1
-    if end == len(sentence):
-        span['end'] = end+1
-    else:
-        span['end'] = end+2
-#         lenth = lenth+1
+    if start == 0: span['start'] = 0
+    else: span['start'] = start -1
+    if end == len(sentence): span['end'] = end+1
+    else: span['end'] = end+2
     return span
 
 
-# In[13]:
+# In[14]:
 
 
 class LSTMTagger(nn.Module):
@@ -216,9 +220,12 @@ class LSTMTagger(nn.Module):
 
     def forward(self, sentence, targetpositions):
         embeds = self.word_embeddings(sentence)
-        embeds.cuda()
+        if usingGPU:
+            embeds = embeds.view(len(sentence), 1, -1)
+        else:
+            embeds = embeds.view(len(sentence), 1, -1).cuda()
         lstm_out_1, self.hidden_lstm_1 = self.lstm_1(
-            embeds.view(len(sentence), 1, -1), self.hidden_lstm_1)
+            embeds, self.hidden_lstm_1)
 
         span = get_target_span(sentence, targetpositions)  
         target_lstm = lstm_out_1[span['start']:span['end']]
@@ -228,7 +235,6 @@ class LSTMTagger(nn.Module):
         
         target_vec = lstm_out_2[-1]
 
-        #tag_space = self.target2hidden(target_vec.view(len(targetpositions), -1))
         tag_space = self.target2hidden(target_vec)
         tag_space = F.relu(tag_space)
         tag_space = self.hidden2tag(tag_space)
@@ -237,27 +243,17 @@ class LSTMTagger(nn.Module):
         return tag_scores
 
 
-# In[14]:
+# In[15]:
 
-
-device = torch.device("cuda:0")
 
 model = LSTMTagger(len(word_to_ix), len(frame_to_ix))
-model.cuda()
+if usingGPU:
+    model.cuda()
+else:
+    pass
+
 loss_function = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-
-model.zero_grad()
-model.hidden = model.init_hidden()
-# with torch.no_grad():
-#     sentence, pos, frame = prepare_sentence(training_data[0])
-#     targetpositions = get_targetpositions(training_data[0])
-#     inputs = prepare_sequence(sentence, word_to_ix)
-#     model.zero_grad()
-#     tag_scores = model(inputs,targetpositions)
-#     print('before')
-#     print(tag_scores)
-#     print('\n')
 
 total_step = len(training_data)
 for epoch in range(NUM_EPOCHS):
